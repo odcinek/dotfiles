ab89e50e7864b6c72f9ce8c11d3239e36ea94580
Marcin Sawicki <odcinek@gmail.com>
2013-10-07 16:36:40 +0200

    Initial commit

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,4 @@ ln -vis ~/.dotfiles/.vimrc ~/.vimrc
 ln -vis ~/.dotfiles/.vim ~/.vim
 ln -vis ~/.dotfiles/.bashrc ~/.bashrc
 ln -vis ~/.dotfiles/.tmux.conf ~/.tmux.conf
+ln -vis ~/.dotfiles/.env ~/.env
