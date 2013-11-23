cfcbcbac1a1a2828048fc5cf409f16b40ba5e863
Marcin Sawicki <odcinek@gmail.com>
2013-11-22 22:42:20 -0800

    Make deploy idempotent
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    CC:
    
    Task ID: #
    
    Blame Rev:

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 set -e 
-set -o verbose
-ln -vis ~/.dotfiles/.vimrc ~/.vimrc
-ln -vis ~/.dotfiles/.vim ~/.vim
-ln -vis ~/.dotfiles/.bashrc ~/.bashrc
-ln -vis ~/.dotfiles/.tmux.conf ~/.tmux.conf
-ln -vis ~/.dotfiles/.env ~/.env
-ln -vis ~/.dotfiles/.gitconfig ~/.gitconfig
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-wget https://github.com/Lokaltog/powerline-fonts/raw/master/Inconsolata/Inconsolata%20for%20Powerline.otf
+
+for path in .vimrc .vim .bashrc .tmux.conf .env .gitconfig; do
+  echo $path;
+  if [ ! -h ~/$path ]; then
+    ln -vis ~/.dotfiles/$path ~/$path
+  fi
+done
+
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle || true
+wget https://github.com/Lokaltog/powerline-fonts/raw/master/Inconsolata/Inconsolata%20for%20Powerline.otf -O ~/.env/Inconsolata%20for%20Powerline.otf
 vim +BundleInstall +qall
