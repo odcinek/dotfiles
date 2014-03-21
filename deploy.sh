189f13a6e35eaf5cd166862161745b598ed9b8fd
Marcin Sawicki <odcinek@fb.com>
2014-03-20 22:40:09 -0700

    Cleanup vim

--- deploy.sh
+++ deploy.sh
@@ -12,5 +12,4 @@ done
 rm -rf ~/.vim/bundle/vundle && git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle || true
 wget --no-check-certificate https://github.com/Lokaltog/powerline-fonts/raw/master/Inconsolata/Inconsolata%20for%20Powerline.otf -O ~/.env/Inconsolata%20for%20Powerline.otf
 vim +BundleInstall +qall
-clear
 echo "Deploy done" 
